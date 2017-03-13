9a23625b39e8c0af0af1a63cdf0316c20b558a06
yasushiyoshida <ysdyss0617@yahoo.co.jp>
2017-03-13 14:43:27 +0900

    root/index等変更

--- app/Controller/RootController.php
+++ app/Controller/RootController.php
@@ -2,28 +2,28 @@
 App::uses('AppController', 'Controller');
 class RootController extends AppController {
 	public $name = 'Root';
-	public $uses = array(
-		'Area',
-		'Pref',
-		'Club',
-		'City'
-	);
+	// public $uses = array(
+	// 	'Area',
+	// 	'Pref',
+	// 	'Club',
+	// 	'City'
+	// );
 
 	public function index() {
-		// エリア一覧取得
-		$this->getAreas();
-		$this->pageInit();
-		$this->set([
-			'title' => '交際クラブを徹底比較',
-			'description' => DESCRIPTION,
-			// 'data_area' => $data_areas,
-			'right_column' => [
-				'side_ad'
-			],
-			'left_column' => [
-				'side_area_nav'
-			]
-		]);
-		$this->layout = 'Pane3';
+		// // エリア一覧取得
+		// $this->getAreas();
+		// $this->pageInit();
+		// $this->set([
+		// 	'title' => '交際クラブを徹底比較',
+		// 	'description' => DESCRIPTION,
+		// 	// 'data_area' => $data_areas,
+		// 	'right_column' => [
+		// 		'side_ad'
+		// 	],
+		// 	'left_column' => [
+		// 		'side_area_nav'
+		// 	]
+		// ]);
+		// $this->layout = 'Pane3';
 	}
 }
