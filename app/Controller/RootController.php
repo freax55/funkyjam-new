5b5f04560178a9912d11e3238fcd76ed0627cee1
yasushiyoshida <ysdyss0617@yahoo.co.jp>
2017-04-10 14:57:01 +0900

    固定ページscout studio

--- app/Controller/RootController.php
+++ app/Controller/RootController.php
@@ -63,19 +63,57 @@ class RootController extends AppController {
 	}
 
 	public function scout() {
+		// $this->pageInit();
+		// $this->set([
+		// 	'title' => 'fankyjam',
+		// 	'description' => 'Funky Jam（ファンキージャム）は久保田利伸、浦嶋りんこ、森大輔、BROWN EYED SOULが所属する芸能プロダクション。弊社ではシンガー・ソングライター、ヴォーカリスト、作曲家、作詞家への志望者を募集しております。真剣にプロを目指す方であればどなたでも応募下さい。',
+		// ]);
+		// $this->render('contents');
+		$action = $this->params->params['action'];
+		$content_company = $this->Postmeta->getPostPages($action);
 		$this->pageInit();
+		$_action = Inflector::camelize($this->params->params['action']);
+		$this->topicPath(
+			[
+				$_action
+			],
+			[
+				'/'
+			]
+		);
+		
 		$this->set([
+			'content' => isset($content_company[0]) ? $content_company[0]:null,
 			'title' => 'fankyjam',
-			'description' => 'Funky Jam（ファンキージャム）は久保田利伸、浦嶋りんこ、森大輔、BROWN EYED SOULが所属する芸能プロダクション。弊社ではシンガー・ソングライター、ヴォーカリスト、作曲家、作詞家への志望者を募集しております。真剣にプロを目指す方であればどなたでも応募下さい。',
+			'description' => 'Funky Jam（ファンキージャム）は久保田利伸、浦嶋りんこ、森大輔、BROWN EYED SOULが所属する芸能プロダクション。アーティストのマネージメント、プロモーションを担当して頂ける人材を探しております。応募される方は履歴書(3カ月以内に撮影した顔写真貼付)と職務経歴書をご郵送下さい。',
 		]);
 		$this->render('contents');
 	}
 
 	public function studio() {
+		// $this->pageInit();
+		// $this->set([
+		// 	'title' => 'fankyjam',
+		// 	'description' => 'Funky Jam（ファンキージャム）は久保田利伸、浦嶋りんこ、森大輔、BROWN EYED SOULが所属する芸能プロダクション。弊社アーティストが使用するレコーディングスタジオ、THE BASEMENT of Funky Jamですが、プロユースのスタジオとして、レンタルのお申込みを承っております。',
+		// ]);
+		// $this->render('contents');
+		$action = $this->params->params['action'];
+		$content_company = $this->Postmeta->getPostPages($action);
 		$this->pageInit();
+		$_action = Inflector::camelize($this->params->params['action']);
+		$this->topicPath(
+			[
+				$_action
+			],
+			[
+				'/'
+			]
+		);
+		
 		$this->set([
+			'content' => isset($content_company[0]) ? $content_company[0]:null,
 			'title' => 'fankyjam',
-			'description' => 'Funky Jam（ファンキージャム）は久保田利伸、浦嶋りんこ、森大輔、BROWN EYED SOULが所属する芸能プロダクション。弊社アーティストが使用するレコーディングスタジオ、THE BASEMENT of Funky Jamですが、プロユースのスタジオとして、レンタルのお申込みを承っております。',
+			'description' => 'Funky Jam（ファンキージャム）は久保田利伸、浦嶋りんこ、森大輔、BROWN EYED SOULが所属する芸能プロダクション。アーティストのマネージメント、プロモーションを担当して頂ける人材を探しております。応募される方は履歴書(3カ月以内に撮影した顔写真貼付)と職務経歴書をご郵送下さい。',
 		]);
 		$this->render('contents');
 	}
