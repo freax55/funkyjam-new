4f5f3ba7a97e64b1096cda780a338ffbf7c9b168
yasushiyoshida <ysdyss0617@yahoo.co.jp>
2017-05-23 14:22:24 +0900

    fanclubエラー修正

--- app/Controller/FanclubController.php
+++ app/Controller/FanclubController.php
@@ -86,7 +86,7 @@ class FanclubController extends AppController {
 		// } else {			
 		// 	$this->redirect('/contact/');
 			// $this->render('add');
-		}
+		// }
 	}
 
 	public function complete() {
